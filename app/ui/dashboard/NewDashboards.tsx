e96b27fd1db23b662f34b09f31a07a94bd680702
Jorge Madson <jorgemadsonsv@gmail.com>
2024-02-14 23:04:28 -0300

    Adicionando logo acme

--- app/ui/dashboard/NewDashboards.tsx
+++ app/ui/dashboard/NewDashboards.tsx
@@ -4,6 +4,8 @@ import { signOut } from "@/auth";
 import { useState } from "react";
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { logOut } from "@/app/lib/actions";
+import AcmeLogo from "../acme-logo";
+import Link from "next/link";
 
 
 interface NewDashboardProps {
@@ -18,22 +20,30 @@ export default function NewDashboard({ dashboards }: NewDashboardProps) {
 
     return <>
         <div className="w-full flex-none md:w-64">
+            <Link
+                className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+                href="/"
+            >
+                <div className="w-32 text-white md:w-40">
+                    <AcmeLogo />
+                </div>
+            </Link>
             {dashboards?.map((d, index) => <button
-                className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+                className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
                 key={d.link} onClick={() => setActiveDash(index)}>
                 {d.name}
             </button>)}
             <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form
-        action={async ()=>{
-          await logOut();
-        }}
-        >
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
+            <form
+                action={async () => {
+                    await logOut();
+                }}
+            >
+                <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                    <PowerIcon className="w-6" />
+                    <div className="hidden md:block">Sign Out</div>
+                </button>
+            </form>
         </div>
         <div className="flex-grow p-1 md:overflow-y-auto ">
             <iframe className="w-full h-full" src={dashboards[activeDash].link || ""}></iframe>
